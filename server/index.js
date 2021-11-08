427cfe357c2d088526698abbb1ea57246682e5de
Hyo Kim <hyo.kim968@gmail.com>
2021-11-08 11:24:19 -0800

    server.js changed a bit

--- server/index.js
+++ server/index.js
@@ -6,18 +6,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '..','public')));
 
 // routing to apis
 app.use('/api', require('./api'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '..', 'public/index.html')));
 
 
+// sends index.html
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+})
 
 app.use((err, req, res) => {
+  console.error(err)
+  console.error(err.stack)
   res.status(err.status || 500).send(err.message || 'Internal Server Error');
 });
 
