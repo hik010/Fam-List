fa7d8e05454ca8596d41f99006e9f54298553387
Hyo Kim <hyo.kim968@gmail.com>
2021-11-08 11:00:04 -0800

    updated config for database.url in db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,12 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/async-week-project', {
-  logging: false
-})
+const config = {logging: false}
+if(process.env.DATABASE_URL){
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  };
+}
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/async-week-project', config)
 
 module.exports = db;
